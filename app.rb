d01bd0a198164941c76f4614226c34592ac9ced7
ADIL NAIMI <adilnm23@hotmail.com>
2019-05-28 04:26:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,19 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  get '/newteam' do
+    erb :newteam
+  end
 
+  post '/team' do
+    @teamname=params["TN"]
+    @coach=params["coach"]
+    @pointguard=params["PG"]
+    @shootingguard=params["SG"]
+    @smallforward=params["SF"]
+    @powerforward=params["PF"]
+    @center=params["center"]
+    erb :team
+  end
 
 end
